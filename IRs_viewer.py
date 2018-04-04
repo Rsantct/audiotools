1f357cd1836ea45ded2f6f1a12314994d10ba6ce
Rsantct <rsantct@gmail.com>
2018-04-04 16:49:52 +0200

    Update IRs_viewer.py

--- IRs_viewer.py
+++ IRs_viewer.py
@@ -94,7 +94,7 @@ def prepara_eje_frecuencias(ax):
 def preparaGraficas():
     columnas = len(IRs)
     top_dBs = 5
-    range_dBs = 60
+    range_dBs = 65
     
     global fig, grid, axMag, axDrv, axPha, axGD, axIR
     #-------------------------------------------------------------------------------
