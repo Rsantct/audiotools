0f2ebe4cdda96fc7720faddaa6084e5d12e64b77
Rsantct <rsantct@gmail.com>
2018-04-08 23:48:21 +0200

    Update actualiza_audiotools-master.sh

--- actualiza_audiotools-master.sh
+++ actualiza_audiotools-master.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
 cd ~/
-touch master.zip
-rm master.zip*
+
+# Borramos si hubiera algun master.zip
+if [[ -f master.zip ]]; then
+    rm master.zip*
+
+# Bajamos el zip de GitHUb
 wget https://github.com/Rsantct/audiotools/archive/master.zip
+# Descomprimos (se descomprime en audiotools-master)
 unzip -o master.zip
+
+# Borramos lo antiguo
+rm -rf ~/auditools
+
+# Y renombramos el directorio descomprimido 
 mv audiotools-master audiotools
+
+# Hacemos ejecutables los archivos
 chmod +x audiotools/*
+
