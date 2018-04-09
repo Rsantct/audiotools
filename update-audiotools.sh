bed512ae5a8f7336969dbd5d9a8935074d4ae8d2
Rsantct <rsantct@gmail.com>
2018-04-09 08:28:54 +0000

    Update update-audiotools.sh

--- update-audiotools.sh
+++ update-audiotools.sh
@@ -3,7 +3,7 @@
 cd ~/
 
 # Borramos si hubiera algun master.zip
-rm -f master.zip*
+rm -f ~/master.zip*
 
 # Bajamos el zip de GitHUb
 wget https://github.com/Rsantct/audiotools/archive/master.zip
@@ -14,14 +14,14 @@ unzip -o master.zip
 rm -rf ~/audiotools
 
 # Y renombramos el directorio descomprimido
-mv audiotools-master audiotools
+mv ~/audiotools-master /audiotools
 
 # Hacemos ejecutables los archivos
-chmod +x audiotools/*
+chmod +x ~/audiotools/*
 
 # Incluimos auditools en el profile del usuario
 profileFile=$(ls -a .*profile*)
-if ! grep -q "bin" "$profileFile"; then
+if ! grep -q "audiotools" "$profileFile"; then
     export PATH=$PATH:$HOME/audiotools
     echo "export PATH=$PATH:$HOME/audiotools" >> profileFile
 fi
