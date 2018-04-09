342ba02c851316d0a65c8a9695936c4501261fff
Rsantct <rsantct@gmail.com>
2018-04-09 18:22:22 +0200

    Update update-audiotools.sh

--- update-audiotools.sh
+++ update-audiotools.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
+# v0.2
+# admite actualizar otra branch distinta de 'master'
+
+branch=master
+if $1; then
+    branch=$1
 
 cd ~/
 
 # Borramos si hubiera algun master.zip
-rm -f ~/master.zip*
+rm -f ~/$branch.zip*
 
 # Bajamos el zip de GitHUb
-wget https://github.com/Rsantct/audiotools/archive/master.zip
+wget https://github.com/Rsantct/audiotools/archive/$branch.zip
 # Descomprimos (se descomprime en audiotools-master)
-unzip -o master.zip
+unzip -o $branch.zip
 
 # Borramos lo antiguo
 rm -rf ~/audiotools
 
 # Y renombramos el directorio descomprimido
-mv ~/audiotools-master ~/audiotools
+mv ~/audiotools-$branch ~/audiotools
 
 # Hacemos ejecutables los archivos
 chmod +x ~/audiotools/*
