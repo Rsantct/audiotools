40a6268b41caa061feca4dac6d696287fbdb54fb
Rsantct <rsantct@gmail.com>
2018-04-10 07:06:38 +0000

    Update q2bw.py

--- q2bw.py
+++ q2bw.py
@@ -4,12 +4,13 @@
 import math
 
 def q2bw(Q):
-    """ http://www.rane.com/note167.html#qformula
+    """
+        convierte un valor de Q a BW en octavas
+        http://www.rane.com/note167.html#qformula
         http://www.rane.com/note170.html
     """
     bw = 2.0 / math.log10(2.0) * math.log10( 0.5 * (1/Q + math.sqrt(1/Q**2 + 4)))
     return bw
-
 
 if __name__ == '__main__':
     pass
