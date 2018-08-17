a0759249df42d10f9b346d5af956566530045bb6
Rsantct <rsantct@gmail.com>
2018-08-17 12:43:46 +0000

    Update FIR_filter.py

--- FIR_filter.py
+++ FIR_filter.py
@@ -17,18 +17,18 @@ Uso:
 
 # https://scipy-cookbook.readthedocs.io/items/ApplyFIRFilter.html
 
-# From scipy.signal, lfilter() is designed to apply a discrete IIR filter to a signal, 
-# so by simply setting the array of denominator coefficients to [1.0], 
+# From scipy.signal, lfilter() is designed to apply a discrete IIR filter to a signal,
+# so by simply setting the array of denominator coefficients to [1.0],
 # it can be used to apply a FIR filter. 
 
-# signal.lfilter is designed to filter one-dimensional data. It can take a two-dimensional 
-# array (or, in general, an n-dimensional array) and filter the data in any given axis. 
-# It can also be used for IIR filters, so in our case, we'll pass in [1.0] for the 
+# signal.lfilter is designed to filter one-dimensional data. It can take a two-dimensional
+# array (or, in general, an n-dimensional array) and filter the data in any given axis.
+# It can also be used for IIR filters, so in our case, we'll pass in [1.0] for the
 # denominator coefficients. In python, this looks like:
 # y = lfilter(b, [1.0], x)
 # 
-# To obtain exactly the same array as computed by convolve or fftconvolve (i.e. to get 
-# the equivalent of the 'valid' mode), we must discard the beginning of the array computed 
+# To obtain exactly the same array as computed by convolve or fftconvolve (i.e. to get
+# the equivalent of the 'valid' mode), we must discard the beginning of the array computed
 # by lfilter. We can do this by slicing the array immediately after the call to filter:
 # y = lfilter(b, [1.0], x)[:, len(b) - 1:]
 
