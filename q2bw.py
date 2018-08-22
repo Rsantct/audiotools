e0b82bf9982884ee39addee3af7865cc743791d5
Rsantct <rsantct@gmail.com>
2018-08-22 17:24:34 +0200

    Update q2bw.py

--- q2bw.py
+++ q2bw.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import math
+import numpy as np
 
 def q2bw(Q):
     """
@@ -9,7 +9,7 @@ def q2bw(Q):
         http://www.rane.com/note167.html#qformula
         http://www.rane.com/note170.html
     """
-    bw = 2.0 / math.log10(2.0) * math.log10( 0.5 * (1/Q + math.sqrt(1/Q**2 + 4)))
+    bw = 2.0 / np.log10(2.0) * np.log10( 0.5 * (1/Q + np.sqrt(1/Q**2 + 4)))
     return bw
 
 if __name__ == '__main__':
