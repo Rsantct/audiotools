3c1da2e6fdeac14fbb7b4c42f09e0df3f3bafe7e
Rsantct <rsantct@gmail.com>
2019-11-18 13:08:20 +0100

    Update FIR_filter.py

--- FIR_filter.py
+++ FIR_filter.py
@@ -4,11 +4,11 @@
 v0.1
 Script para combinar dos filtros FIR en formato '.pcm' 32 bits.
 
-Mediante convolución obtenemos el FIR 'filter.pcm'.
+Mediante convolución obtenemos el FIR resultado.
 
 Uso:
 
-    FIR_filter.py   fir_1.pcm   fir_2.pcm
+    FIR_filter.py   path/to/fir_1.pcm   path/to/fir_2.pcm
 
 """
 
@@ -19,14 +19,14 @@ Uso:
 
 # From scipy.signal, lfilter() is designed to apply a discrete IIR filter to a signal,
 # so by simply setting the array of denominator coefficients to [1.0],
-# it can be used to apply a FIR filter. 
+# it can be used to apply a FIR filter.
 
 # signal.lfilter is designed to filter one-dimensional data. It can take a two-dimensional
 # array (or, in general, an n-dimensional array) and filter the data in any given axis.
 # It can also be used for IIR filters, so in our case, we'll pass in [1.0] for the
 # denominator coefficients. In python, this looks like:
 # y = lfilter(b, [1.0], x)
-# 
+#
 # To obtain exactly the same array as computed by convolve or fftconvolve (i.e. to get
 # the equivalent of the 'valid' mode), we must discard the beginning of the array computed
 # by lfilter. We can do this by slicing the array immediately after the call to filter:
@@ -41,7 +41,8 @@ import pydsd
 try:
     xfile = sys.argv[1]
     yfile = sys.argv[2]
-    zfile = "filter.pcm"
+    #zfile = "result.pcm"
+    zfile = xfile.replace('.pcm','') + '+' + yfile.replace('.pcm','') + '.pcm'
 except:
     print (__doc__)
     sys.exit()
@@ -60,3 +61,4 @@ z = w * signal.lfilter(y, [1.0], x)#[:, len(x) - 1:] # este slice no lo entiendo
 
 # Guardamos el resultado en el archivo de salida
 utils.savePCM32(z, zfile)
+print( f'saved: {zfile}' )
