1c9dbcd7583c1fcd0da643a9c2114bf3f45c5249
Rsantct <rsantct@gmail.com>
2020-05-20 18:19:57 +0200

    Update house_curves.py

--- brutefir_eq/house_curves.py
+++ brutefir_eq/house_curves.py
@@ -4,23 +4,26 @@
 
     usage:
 
-    house_curves.py  -RXX  -fsXX  -loN  -loFC -hiFC  --save  --plot
+    house_curves.py  -RXX  -fsXX  -loSX  -loGX -hiGX -loFX -hiFX  --save  --plot
 
         -RXX:   R10 | R20 | R40 | R80  iso R series (default: R20 ~ 1/3 oct)
 
         -fsXX:  44100 | 48000 | 96000  sampling frequency Hz
                 (default: 44100, upper limits RXX to 20000 Hz)
 
-        -loN:   1 | 2  low shelf order (default: N -> 1, 1st order 6 dB/oct)
+        -loSX:  6 | 12  low shelf slope (default: X=6, 1st order 6 dB/oct)
 
-        -loFC:  Low shelf center frequency (default: FC -> 120 Hz)
+        -loFX:  Low shelf center frequency (default: X=120 Hz)
 
-        -hiFC:  High roll-off corner frequency (default: FC -> 500 Hz)
+        -loGX:  Low shelf gain dB (default: 0 to +6.0 dB, 1 dB step)
+
+        -hiFX:  High roll-off corner frequency (default: X=500 Hz)
+
+        -hiAX:  High roll-off end atten dB (default: from 0 to -6.0 dB, 1 dB step)
 
         --save  save curves to disk
 
         --plot
-
 """
 
 import sys
@@ -54,7 +57,8 @@ def make_high(fc, gain):
 
 def doplotexample():
 
-    hc = curves["6-6"]
+    # Getting the last generated curve
+    hc = curves[list(curves.keys())[-1]]
 
     fig, (axMag, axPha) = plt.subplots(2,1)
     fig.set_size_inches(12, 6)
@@ -87,12 +91,11 @@ def doplotall():
     axMag.set_ylim(-8, 8)
     axPha.set_ylim(-45, 45)
 
-    for lo, dB in enumerate(dB_steps):
-        for hi, dB in enumerate(dB_steps):
-            mag = curves[f'{lo}-{hi}']['mag']
-            pha = curves[f'{lo}-{hi}']['pha']
-            axMag.semilogx(f, mag)
-            axPha.semilogx(f, pha)
+    for curve in curves:
+        mag = curves[curve]['mag']
+        pha = curves[curve]['pha']
+        axMag.semilogx(f, mag)
+        axPha.semilogx(f, pha)
 
     plt.show()
 
@@ -102,13 +105,16 @@ def make_curves():
     curves stored in a dictionary
     """
     curves = {}
-    for lo, dB in enumerate(dB_steps):
-        clo = make_low( fc=fc_low, gain=dB )
-        for hi, dB in enumerate(dB_steps):
-            chi = make_high( fc=fc_high, gain=-dB )
+    for lo_gain in lo_gains:
+        clo = make_low( fc=fc_low, gain=lo_gain )
+        for hi_gain in hi_gains:
+            chi = make_high( fc=fc_high, gain=hi_gain )
             hc_mag = clo + chi
             _,_,hc_pha = min_phase_from_real_mag( f, hc_mag)
-            curves[f'{lo}-{hi}'] = {'mag': hc_mag, 'pha': hc_pha}
+            lo_str = str(round(float(lo_gain), 1))
+            hi_str = str(round(float(hi_gain), 1))
+            hi_str = f'-{hi_str}'.replace('--', '-')
+            curves[f'+{lo_str}{hi_str}'] = {'mag': hc_mag, 'pha': hc_pha}
     return curves
 
 
@@ -123,14 +129,13 @@ def save_curves():
     fname = f'{folder}/freq.dat'
     np.savetxt( fname, f.transpose(), fmt='%.4e' )
 
-    for lo, dB in enumerate(dB_steps):
-        for hi, dB in enumerate(dB_steps):
-            mag = curves[f'{lo}-{hi}']['mag']
-            pha = curves[f'{lo}-{hi}']['pha']
-            mname = f'{folder}/+{lo}-{hi}_target_mag.dat'
-            pname = f'{folder}/+{lo}-{hi}_target_pha.dat'
-            np.savetxt( mname, mag, fmt='%.4e' )
-            np.savetxt( pname, pha, fmt='%.4e' )
+    for curve in curves:
+        mag = curves[curve]['mag']
+        pha = curves[curve]['pha']
+        mname = f'{folder}/{curve}_target_mag.dat'
+        pname = f'{folder}/{curve}_target_pha.dat'
+        np.savetxt( mname, mag, fmt='%.4e' )
+        np.savetxt( pname, pha, fmt='%.4e' )
 
     print(f'freqs saved to:  {folder}/freq.dat')
     print(f'curves saved to: {folder}/L-H_target_mag.dat')
@@ -150,6 +155,8 @@ if __name__ == '__main__':
     # Will generate a set of curves by combining low shelf and house ranges
     dB_range  = 6       # use integer
     dB_step   = 1       # use integer
+    lo_gains  = []      # the wanted gains for low shelf
+    hi_gains  = []      # the wanted gaings for high roll-off
 
     # Frequency bins
     fmin    = 10
@@ -164,49 +171,81 @@ if __name__ == '__main__':
         print(__doc__)
         sys.exit()
     for opc in sys.argv[1:]:
+
         if opc == '-h' or opc == '--help':
             print(__doc__)
             sys.exit()
+
         elif opc[:2] == '-R':
             Rseries = opc[1:]
+
         elif opc[:3] == '-fs':
             value = int(opc[3:])
             if value in (44100, 48000, 96000):
                 fs = value
-        elif '-lo' in opc:
-            value = int(opc[3:])
-            if value in (1, 2):
-                shelf_order = value
-            elif value >= 20 and value <= 250:
+
+        elif '-loS' in opc:
+            value = int(opc[4:])
+            if value in (6, 12):
+                shelf_order = {6:1, 12:2}[value]
+            else:
+                raise ValueError('Low self slope choose 6 or 12 (dB/oct)')
+
+        elif '-loF' in opc:
+            value = int(opc[4:])
+            if value >= 20 and value <= 250:
                 fc_low = float(value)
             else:
                 raise ValueError('Low self center freq out of range')
-        elif '-hi' in opc:
-            value = int(opc[3:])
-            if value >= 250 and value <= 10000:
-                fc_high = float(value)
+
+        elif '-loG' in opc:
+            value = float(opc[4:])
+            if value >= 0.0 and value <= 12.0:
+                lo_gains = [value]
             else:
-                raise ValueError('Hi roll-off corner out of range')
+                raise ValueError('Low self gain between 0 ... 12 dB')
+
+        elif '-hiF' in opc:
+            value = int(opc[4:])
+            if value >= 250 and value <= 10000:
+                fc_low = float(value)
+            else:
+                raise ValueError('Hi roll-off corner between 250 ... 10000 Hz')
+
+        elif '-hiA' in opc:
+            value = float(opc[4:])
+            if value >= 0.0 and value <= 12.0:
+                hi_gains = [-value]
+            else:
+                raise ValueError('Hi roll-off between 0 ... 12 dB')
+
         elif opc == '--save' or opc == '-s':
             savetodisk = True
+
         elif '-p' in opc:
             plot = True
 
 
     f = get_iso_R(Rseries, fmin=fmin, fs=fs)
-    shelf_slope = {1:'6 dB/oct', 2:'12 dB/oct'}[shelf_order]
+    shelf_slope_info = {1:'6 dB/oct', 2:'12 dB/oct'}[shelf_order]
 
     print(f'Using {Rseries} iso frequencies'
           f' from {int(f[0])} Hz to {int(f[-1])} Hz (fs: {fs})')
-    print(f'Low shelf center freq: {fc_low} Hz, slope: {shelf_slope}')
+    print(f'Low shelf center freq: {fc_low} Hz, slope: {shelf_slope_info}')
     print(f'High roll-off corner: {fc_high} Hz')
 
-    dB_steps = np.arange(0, dB_range + dB_step, dB_step)
+    # If not a gain is selected, will make set of curves (i.e 0...6 dB)
+    if not lo_gains:
+        lo_gains = np.arange(0, dB_range + dB_step, dB_step)
+    if not hi_gains:
+        hi_gains = np.arange(0, dB_range + dB_step, dB_step) * -1
+
     print('cumputing curves...')
     curves   = make_curves()
 
     if plot:
         doplotexample()
+        #doplotall()
 
     if savetodisk:
         save_curves()
