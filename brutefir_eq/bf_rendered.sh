987a32baeea9e87fb5392166751730284ed402e4
Rsantct <rsantct@gmail.com>
2020-05-21 19:58:30 +0200

    Update bf_rendered.sh

--- brutefir_eq/bf_rendered.sh
+++ brutefir_eq/bf_rendered.sh
@@ -2,11 +2,12 @@
 
 if [[ $1 ]]; then
     fs=$1
-    "$HOME"/audiotools/IRs_viewer.py "/tmp/brutefir-rendered-0.txt" $fs -pha -pdf
+    "$HOME"/audiotools/IRs_viewer.py "/tmp/brutefir-rendered-0.txt" \
+                                     $fs -pha -lptol=-60
 
 else
     echo
-    echo "Plots the rendered impulse given from the Brutefir logic eq runtime module"
+    echo "Plots the rendered impulse given from Brutefir eq logic runtime module"
     echo "at /tmp/brutefir-rendered-0.txt"
     echo
     echo "Usage:bf_rendered.sh fs"
