63fb6b0c6671e6b55bdc03129bd3ae689a3fc479
Rsantct <rsantct@gmail.com>
2020-09-02 21:21:00 +0200

    Update REWwav2pcm.py

--- REWwav2pcm.py
+++ REWwav2pcm.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
     v0.1beta
-    
+
     Convierte una IR de DRC originada en REW Room EQ Wizard
     en un archivo raw float32 (.pcm) adecuado para un convolver
     como Brutefir
@@ -10,8 +10,8 @@
     NOTA:   Los impulsos IR .wav de REW vienen con el pico desplazado
             tantas muestras como el valor de la Fs del wav.
             Aquí haremos una reconstrucción completa:
-            time domain > spectrum > time domain, tomando la 
-            minimum phase descartamos el exceso de phase si lo hubiera, 
+            time domain > spectrum > time domain, tomando la
+            minimum phase descartamos el exceso de phase si lo hubiera,
             resultando en un impulso con el pico al inicio.
 """
 
@@ -27,15 +27,15 @@ if __name__ == "__main__":
         sys.exit()
 
     # taps de salida deseados (PDTE PASAR COMO ARGUMENTO)
-    m = 2 ** 15 
+    m = 2 ** 15
 
     # Archivos de entrada y de salida
     fin  = sys.argv[1]
     fout = fin.replace(".wav", ".pcm")
 
     # Leemos el impulso de entrada imp1
-    fs, imp1 = tools.readWAV16(fin)
-    
+    fs, imp1 = tools.readWAV(fin)
+
     # Espectro completo
     h = np.fft.fft(imp1)
     # Réplica del espectro completo en fase mínima
